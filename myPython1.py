c40252b8f8b004738721dacf21c1e01bbbaede9e
otniel5 <otniel.c95@gmail.com>
2021-03-04 21:19:23 +0200

    commit 3

--- myPython1.py
+++ myPython1.py
@@ -11,3 +11,11 @@ name = input("what is your name?")
 print(f"welcome to you {name}")
 
 print(f"{name.title()} your name has{len(name)} letters")
+
+for index , letter in enumerate(name):
+    print(f"{index +1}: {letter}") 
+    if letter == "o":
+        print("😉")
+    else:
+        print("🤦‍♂️")    
+
